80db5ef06a45eeae1b0c41b9d311758799c47599
Brian Koekkoek <bjkoekkoek@hotmail.com>
2022-01-18 12:46:03 +0800

    Added input/checkBtn functionality

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 'use strict'
 
-console.log(document.querySelector('.message').textContent)
+const checkBtn = document.querySelector('.checkBtn')
+
+checkBtn.addEventListener('click', () => {
+  const input = Number(document.querySelector('.inputValue').value)
+  if(!input){
+    document.querySelector('.message').textContent = '⛔️ No number!'
+  } else {
+    document.querySelector('.message').textContent = `You have guessed ${input}..`
+  }
+})
