23164052e33e5dda8c0479e3314f791783f209c3
Brian Koekkoek <bjkoekkoek@hotmail.com>
2022-01-19 14:35:18 +0800

    Refactor code

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const createNumber = () => {
   randomNumber = Math.floor(Math.random() * 20) + 1
 }
 
+const displayMessage = (message) => {
+  document.querySelector('.message').textContent = message
+}
+
 const checkBtn = document.querySelector('.checkBtn')
 const againBtn = document.querySelector('.againBtn')
 let score = 20
@@ -14,49 +18,41 @@ let hiscore = 0
 checkBtn.addEventListener('click', () => {
   const input = Number(document.querySelector('.inputValue').value)
   if(!input){
-    document.querySelector('.message').textContent = '⛔️ No number!'
+    displayMessage('⛔️ No number!')
   } else if (input > 20 || input < 1){
-    document.querySelector('.message').textContent = 'Hint: Between 1 - 20'
+    displayMessage('Hint: Between 1 - 20')
   } else if (input === randomNumber){
+    displayMessage('You have guessed it! 🥳🎉🥳')
     document.querySelector('body').style.backgroundColor = '#60b347'
     document.querySelector('.secretNumber').style.width = '200px'
-    document.querySelector('.message').textContent = 'You have guessed it! 🥳🎉🥳'
     document.querySelector('.secretNumber').textContent = randomNumber
     hiscore = document.querySelector('.score').textContent
     if (hiscore > document.querySelector('.hiScore').textContent){
       document.querySelector('.hiScore').textContent = hiscore
     }
-  } else if (input > randomNumber){
-      if (score > 1){
-        document.querySelector('.message').textContent = 'Hint:  📈 Too high..'
-        score--
-        document.querySelector('.score').textContent = score
-      } else {
-        document.querySelector('.score').textContent = 0
-        document.querySelector('.message').textContent = '💥 Score 0! Again?'
-      }
-  } else if (input < randomNumber){
-      if (score > 1){
-        document.querySelector('.message').textContent = 'Hint: 📉 Too low..'
-        score--
-        document.querySelector('.score').textContent = score
-      } else {
-        document.querySelector('.score').textContent = 0
-        document.querySelector('.message').textContent = '💥 Score 0! Again?'
-      }
+  } else if (input !== randomNumber){
+    if (score > 1){
+      displayMessage(input > randomNumber ? 'Hint:  📈 Too high..'  : 'Hint: 📉 Too low..')
+      score--
+      document.querySelector('.score').textContent = score
+    } else {
+      displayMessage('💥 Score 0! Again?')
+      document.querySelector('.score').textContent = 0
+      document.querySelector('.secretNumber').textContent = randomNumber
+      document.querySelector('body').style.border = '5px solid red'
+      document.querySelector('body').style.backgroundColor = 'rgb(112, 108, 108)'
     }
+  }
 })
 
 againBtn.addEventListener('click', () => {
   createNumber()
-  console.log(randomNumber)
+  displayMessage('Start guessing...')
+  score = 20
   document.querySelector('.secretNumber').textContent = '?'
   document.querySelector('.inputValue').value = ''
-  document.querySelector('.message').textContent = 'Start guessing...'
+  document.querySelector('.secretNumber').style.width = '100px'
   document.querySelector('.score').textContent = 20
   document.querySelector('body').style.backgroundColor = '#222'
-      document.querySelector('.secretNumber').style.width = '100px'
-  score = 20
+  document.querySelector('body').style.border = 'none'
 })
-
-console.log(randomNumber)
