96db03bf4b0f10924add78e61443294f6263c03d
Brian Koekkoek <bjkoekkoek@hotmail.com>
2022-01-19 12:39:45 +0800

    Added score, hiscore, end game

--- script.js
+++ script.js
@@ -8,20 +8,37 @@ const createNumber = () => {
 
 const checkBtn = document.querySelector('.checkBtn')
 const againBtn = document.querySelector('.againBtn')
+let score = 20
 
 checkBtn.addEventListener('click', () => {
   const input = Number(document.querySelector('.inputValue').value)
   if(!input){
     document.querySelector('.message').textContent = '⛔️ No number!'
+  } else if (input > 20 || input < 1){
+    document.querySelector('.message').textContent = 'Hint: Between 1 - 20'
   } else if (input === randomNumber){
-    document.querySelector('.message').textContent = '🎊 You have guessed it!'
+    document.querySelector('.message').textContent = 'You have guessed it! 🥳🎉🥳'
     document.querySelector('.secretNumber').textContent = randomNumber
-    document.querySelector('.hint').textContent = '🥳🎉🥳'
+    document.querySelector('.hiScore').textContent = document.querySelector('.score').textContent
   } else if (input > randomNumber){
-    document.querySelector('.hint').textContent = 'Hint:  📈 Too high..'
+      if (score > 1){
+        document.querySelector('.message').textContent = 'Hint:  📈 Too high..'
+        score--
+        document.querySelector('.score').textContent = score
+      } else {
+        document.querySelector('.score').textContent = 0
+        document.querySelector('.message').textContent = '💥 Score 0! Again?'
+      }
   } else if (input < randomNumber){
-    document.querySelector('.hint').textContent = 'Hint: 📉 Too low..'
-  }
+      if (score > 1){
+        document.querySelector('.message').textContent = 'Hint: 📉 Too low..'
+        score--
+        document.querySelector('.score').textContent = score
+      } else {
+        document.querySelector('.score').textContent = 0
+        document.querySelector('.message').textContent = '💥 Score 0! Again?'
+      }
+    }
 })
 
 againBtn.addEventListener('click', () => {
@@ -30,7 +47,8 @@ againBtn.addEventListener('click', () => {
   document.querySelector('.secretNumber').textContent = '?'
   document.querySelector('.inputValue').value = ''
   document.querySelector('.message').textContent = 'Start guessing...'
-  document.querySelector('.hint').textContent = 'Hint: -'
+  document.querySelector('.score').textContent = 20
+  score = 20
 })
 
 console.log(randomNumber)
